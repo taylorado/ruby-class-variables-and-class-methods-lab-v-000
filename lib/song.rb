ff480494e10ecd32cda6216ecc283f2303a1ae79
Taylor Donlan <taylordonlan@gmail.com>
2018-03-09 16:34:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,19 @@
 class Song
 
   @@count = 0
-
+  @@artists = []
+  @@genres =[]
+  
   def initialize(name, artist, genre)
       @@count += 1
     end
 
   def self.count
     @@count
+  end
+
+  def name
+    self.name
   end
 
 end
